2fa14a639184128aec787fe0ed68493b55457929
jcserv <jarrod.servilla@mail.utoronto.ca>
2020-10-17 11:29:08 -0400

    Makefile setup

--- admin/SQAM_App/models.py
+++ admin/SQAM_App/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
+
 class Job(models.Model):
-    Assignment_Name = models.CharField(max_length=50)
-    Max_Marks = models.IntegerField()
-    Create_Tables = models.FileField(upload_to='uploads/%Y/%m/%d/')
-    Load_Data = models.FileField(upload_to='uploads/%Y/%m/%d/')
-    # TODO Add Required Feilds
-    
+    assignment_name = models.CharField(max_length=50)
+    max_marks = models.IntegerField()
+    create_tables = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    load_data = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    # TODO Add Required Fields
+
     class Meta:
         ordering = ['id']
+
 
 class MarkusJob(Job):
-    MarkusSetting1 = models.CharField(max_length=50)
-    MarkUsSetting2 = models.CharField(max_length=50)
-    # TODO Add Required Feilds
+    markus_setting1 = models.CharField(max_length=50)
+    markus_setting2 = models.CharField(max_length=50)
+    # TODO Add Required Fields
 
     class Meta:
         ordering = ['id']
-
-
--- admin/SQAM_App/tests.py
+++ admin/SQAM_App/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from SQAM_App.models import MarkusJob
 
-# Create your tests here.
+
+class MarkUsJobObjectCreationTest(TestCase):
+    def setUp(self):
+        MarkusJob.objects.create(assignment_name="CSC343 A1", max_marks=100,
+                                 markus_setting1="Setting1", markus_setting2="Setting2")
+
+    def test_data(self):
+        job1 = MarkusJob.objects.get(assignment_name="CSC343 A1")
+        self.assertEqual(job1.assignment_name, "CSC343 A1")

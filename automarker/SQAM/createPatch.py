f69e7a39231b70f4cb113c9e6b282d562b682f68
Vaishvik <vaishvik.maisuria@gmail.com>
2020-11-17 22:11:38 -0500

    done

---
+++ automarker/SQAM/createPatch.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import csv
+import random
+
+PATHOUTPUT = "./Demo/Student_Information_and_Submissions/"
+PATHSUBMISSION = "./Demo/Submissions/"
+
+def createFiles(path):
+    print("Start")
+    fd = open(path + "dirs_and_names.txt", "w")
+    fg = open(path + "groups.txt", "w")
+    fs = open(path + "students.csv", "w")
+    return fd, fg, fs
+
+if __name__ == "__main__":
+    submissions, outputPath, inputPath = "", "", ""
+    fd, fg, fs = None, None, None
+    if len(sys.argv) == 1:
+        print("Start NO INPUT")
+        outputPath = PATHOUTPUT
+        inputPath = PATHSUBMISSION
+    elif len(sys.argv) == 3:
+        print("Start INPUT")
+        outputPath = sys.argv[1]
+        inputPath = sys.argv[2]
+        
+    fd, fg, fs = createFiles(outputPath)
+    submissions = os.listdir(inputPath)
+    writer = csv.writer(fs)
+
+    for count, name in enumerate(submissions):
+        fd.write("./Demo/Submissions/{},{}\n".format(name, name))
+        fg.write("{},{},student{}\n".format(name, name, count))
+        writer.writerow(["studentID{}".format(count), "studentName{}".format(count), "studentlast{}".format(count), "{}{}{}{}{}".format(random.randrange(10), random.randrange(10), random.randrange(10), count, count), "email"])
+    
+    fd.close()
+    fg.close() 
+    fs.close()

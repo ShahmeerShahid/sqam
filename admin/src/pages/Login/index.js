49f7ea8ffd5081c1df8904b22bccea6782dc58d5
zhoux126 <terry.zhou@utoronto.ca>
2020-11-21 00:26:07 +0800

    made some of the functionalities more reasonable

--- admin/src/pages/Login/index.js
+++ admin/src/pages/Login/index.js
@@ -7,28 +7,34 @@ import {
     Input,
     Flex,
 	Heading,
-	Link
+	Text,
+	CircularProgress
 } from "@chakra-ui/core";
 
 
 import {login} from './loginfunc';
 import ErrorMsg from '../../components/ErrorMsg';
-import SuccessMsg from '../../components/SuccessMsg';
+import { Link } from "react-router-dom";
+
 
 export function LoginForm(){
 		const [username, setUsername] = useState(null);
 		const [password, setPassword] = useState(null);
 		const [error, setError] = useState(null);
-		const [success, setSuccess] = useState(null);
+		const [loading, setLoading] = useState(false);
+		const [loggedIn, setLoggedIn] = useState(false);
 		const handleSubmit = async event => {
 			event.preventDefault();
+			setLoading(true);
 			try {
 			  await login({ username, password });
-			  setSuccess('You have successfully logged in');
+			  setLoggedIn(true);
+			  setLoading(false);
 			} catch (error) {
 			  setError('Invalid username or password');
-			  setUsername('');
-			  setPassword('');
+			  setLoading(false);
+			  setUsername(null);
+			  setPassword(null);
 			}
 		  };
 	return(
@@ -36,32 +42,54 @@ export function LoginForm(){
 		<div className = "myDiv">
 			<Flex w = "full" align = "center" justifyContent = "center" height = "100vh">
 				<Box p = {20} maxWidth = "1000px" borderWidth = {2} borderRadius = {8} boxShadow = "xl">
-				<Box textAlign = "center">
-				<Heading size = "2xl">Login</Heading>
-				</Box>
-				<Box textAlign = "left">
-					<form onSubmit = {handleSubmit}>
-					{error && <ErrorMsg message={error} />}
-					{success && <SuccessMsg message = {success}/>}
-						<FormControl mt = {4}>
-							<FormLabel>Username</FormLabel>
-							<Input type = "username" placeholder = "example@youremail.com"
-							onChange = {event => setUsername(event.currentTarget.value)}/>
-						</FormControl>
-						<FormControl mt = {4}>
-							<FormLabel>Password</FormLabel>
-							<Input type = "password"
-							onChange = {event => setPassword(event.currentTarget.value)}
-							/>
-						</FormControl>
-						<Button w = "full" variantColor = "blue" variant = "solid" mt = {4} type = "submit">
-							Sign in
-						</Button>
-						<Link href = "http://q.utoronto.ca">
-							Forget password?
-						</Link>
-					</form>  
-				</Box>
+				{loggedIn? (
+					<Box textAlign="center">
+					<Text>liutmich logged in!
+					<Button variantColor="orange"
+					  variant="outline"
+					  width="full"
+					  mt={4}
+					>
+					<Link to = "/tasks">
+					  click here to continue
+					</Link>
+					</Button>
+					</Text>
+				  	</Box>
+				
+					) : (
+					<>
+					<Box textAlign="center">
+						<Heading size = "2xl">Login</Heading>
+					</Box>
+					<Box textAlign = "left">
+						<form onSubmit = {handleSubmit}>
+						{error && <ErrorMsg message={error} />}
+							<FormControl mt = {4}>
+								<FormLabel>Username</FormLabel>
+								<Input type = "username" placeholder = "example@youremail.com"
+								onChange = {event => setUsername(event.currentTarget.value)}/>
+							</FormControl>
+							<FormControl mt = {4}>
+								<FormLabel>Password</FormLabel>
+								<Input type = "password"
+								onChange = {event => setPassword(event.currentTarget.value)}
+								/>
+							</FormControl>
+							<Button w = "full" variantColor = "blue" variant = "solid" mt = {4} type = "submit">
+								{loading ? (
+									<CircularProgress isIndeterminate color = "green.400" size="24px"/>
+								) : (
+									'Sign In'
+								)}
+							</Button>
+							<Link to = "/">
+								Forget password?
+							</Link>
+						</form>  
+					</Box>
+					</>
+				)}
 				</Box>	
 			</Flex>
 		</div>

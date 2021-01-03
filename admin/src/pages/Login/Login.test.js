de2f3009da6d3e8c854323604d2dbe53115be2f6
zhoux126 <terry.zhou@utoronto.ca>
2021-01-04 01:40:29 +0800

    use render instead of shallow

--- admin/src/pages/Login/Login.test.js
+++ admin/src/pages/Login/Login.test.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+//import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 import { login } from "../../requests/login/";
 
 jest.mock("../../requests/login");
 describe("Login test", () => {
-  const wrapper = shallow(<Login />);
+  const wrapper = render(<Login />);
 
   it("should have a login button", () => {
     expect(wrapper.find("Button")).toHaveLength(1);

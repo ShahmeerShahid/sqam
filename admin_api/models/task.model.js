83233f7dd7b1c3d0955486cf9348507a88f817f5
Shahmeer Shahid <shahmeer.shahid@utoronto.ca>
2021-02-26 23:40:46 +0000

    Revert "Merge branch 'master' of mcsscm.utm.utoronto.ca:liutmich/sqam into 88-admin-switch-to-using-a-single-init-sql-file"
    This reverts commit 64e3848ebacb4c4c9c8872aa62b3c0008885d137, reversing
    changes made to c57d4ac8daccff8b38d32c5934d4d9575cf45703.

--- admin_api/models/task.model.js
+++ admin_api/models/task.model.js
@@ -4,113 +4,101 @@ const Schema = mongoose.Schema;
 const constants = require("../constants");
 
 const Submission = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: constants.statuses,
-    required: true,
-    default: "Pending",
-  },
+	name: {
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: constants.statuses,
+		required: true,
+		default: "Pending",
+	},
 });
 
 const Log = new Schema({
-  timestamp: Date,
-  text: String,
-  source: {
-    type: String,
-    required: true,
-    enum: constants.logSources,
-  },
+	timestamp: Date,
+	text: String,
+	source: {
+		type: String,
+		required: true,
+		enum: constants.logSources,
+	},
 });
 
 const Task = new Schema(
-  {
-    tid: {
-      type: Number,
-      unique: true,
-      default: 1,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: constants.statuses,
-      required: true,
-      default: "Pending",
-    },
-    connector: {
-      type: String,
-      enum: constants.connectors,
-      default: "markus-connector",
-    },
-    submissions: [Submission],
-    num_submissions: {
-      type: Number,
-      default: 0,
-    },
-    max_marks: {
-      type: Number,
-      default: 0,
-    },
-    max_marks_per_question: [Number],
-    marking_type: {
-      type: String,
-      default: "",
-    },
-    question_names: [String],
-    submission_file_name: {
-      type: String,
-      default: "",
-    },
-    create_tables: {
-      type: String,
-      default: "",
-    },
-    create_trigger: {
-      type: String,
-      default: "",
-    },
-    create_function: {
-      type: String,
-      default: "",
-    },
-    load_data: {
-      type: String,
-      default: "",
-    },
-    solutions: {
-      type: String,
-      default: "",
-    },
-    submissions_path: {
-      type: String,
-      default: "",
-    },
-    timeout: {
-      type: Number,
-      default: 100,
-      max: 300,
-    },
-    db_type: {
-      type: String,
-      default: "mysql",
-    },
-    extra_fields: {},
-    logs: [Log], // Mongoose automatically sets default to []
-  },
-  {
-    timestamps: true,
-  }
+	{
+		tid: {
+			type: Number,
+			unique: true,
+			default: 1,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: constants.statuses,
+			required: true,
+			default: "Pending",
+		},
+		connector: {
+			type: String,
+			enum: constants.connectors,
+			default: "markus-connector",
+		},
+		submissions: [Submission],
+		num_submissions: {
+			type: Number,
+			default: 0,
+		},
+		max_marks: {
+			type: Number,
+			default: 0,
+		},
+		max_marks_per_question: [Number],
+		marking_type: {
+			type: String,
+			default: "",
+		},
+		question_names: [String],
+		submission_file_name: {
+			type: String,
+			default: "",
+		},
+		initFile: {
+			type: String,
+			default: "",
+		},
+		solutions: {
+			type: String,
+			default: "",
+		},
+		submissions_path: {
+			type: String,
+			default: "",
+		},
+		timeout: {
+			type: Number,
+			default: 100,
+			max: 300,
+		},
+		db_type: {
+			type: String,
+			default: "mysql",
+		},
+		extra_fields: {},
+		logs: [Log], // Mongoose automatically sets default to []
+	},
+	{
+		timestamps: true,
+	}
 );
 
 Task.plugin(server.autoIncrement.plugin, { model: "Task", field: "tid" });
 module.exports = {
-  Task: mongoose.model("Task", Task),
-  TaskSchema: Task,
-  Log: mongoose.model("Log", Log),
+	Task: mongoose.model("Task", Task),
+	TaskSchema: Task,
+	Log: mongoose.model("Log", Log),
 };

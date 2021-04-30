8a3e99bd58ddd18d755b513b780b0489fd8d7036
Vaishvik Maisuria <vaishvikmaisuria@Vaishviks-MacBook-Pro.local>
2021-04-29 23:55:50 -0400

    updated documentation for frontend

--- admin/src/requests/connectors/index.js
+++ admin/src/requests/connectors/index.js
@@ -1,10 +1,12 @@
 import { getRequest, getRequestSpecifyPort } from "../../network";
 
+// Gets list of connectors (name and url) that can be used to download submissions
 export async function fetchConnectors() {
   const response = await getRequest("/api/connectors/");
   return response.data;
 }
 
+// 
 export async function fetchExtraFields(connector_name) {
   try {
     const response = await getRequest(
